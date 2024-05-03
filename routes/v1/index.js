96e5b2d3c2f7fcf1f05f09c203ee52ff91715f2c
rizkirobawa <rizkirobawa@gmail.com>
2024-05-03 12:14:01 +0700

    Added swagger-ui

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -4,6 +4,21 @@ const Picture = require("../v1/picture.routes");
 const User = require("../v1/user.routes");
 const Auth = require("../v1/auth.routes");
 
+const swaggerUI = require("swagger-ui-express");
+const yaml = require("yaml");
+const fs = require("fs");
+const path = require("path");
+
+const swagger_path = path.resolve(__dirname, "../../docs/docs-api.yaml");
+const file = fs.readFileSync(swagger_path, "utf-8");
+
+const swaggerDocument = yaml.parse(file);
+router.use(
+  "/api/v1/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument)
+);
+
 router.use("/api/v1", Auth);
 router.use("/api/v1", User);
 router.use("/api/v1", Picture);
