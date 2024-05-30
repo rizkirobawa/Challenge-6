214e2364bd13fe51c6381004ff30c3ca926667d9
rizkirobawa <rizkirobawa@gmail.com>
2024-05-30 15:49:19 +0700

    test deploy

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const router = require("./routes/v1")
 const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use(router)
 
 app.get('/', (req,res) => res.json({status: true, message: "Use endpoint /api/v1/api-docs to access API documentation", data: null}))
